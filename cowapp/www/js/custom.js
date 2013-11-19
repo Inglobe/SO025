ef7577ff786f25bae2f5d099dc5c4b5f80ca4d08
nacho22martin <nacho22martin@gmail.com>
2013-11-19 13:21:18 -0300

    adding last screens and a custom jq script

---
+++ cowapp/www/js/custom.js
@@ -0,0 +1,44 @@
+
+          var oTable;
+          var giRedraw = false;
+
+          $(document).ready(function() {
+            $('#activelist').dataTable( {
+                    "sScrollY": "150px",
+                    "bPaginate": false,
+                    "bInfo": false             
+                } );
+            /* Add a click handler to the rows - this could be used as a callback */
+            $("#activelist tbody").click(function(event) {
+              $(oTable.fnSettings().aoData).each(function (){
+                $(this.nTr).removeClass('row_selected');
+              });
+              $(event.target.parentNode).addClass('row_selected');
+            });
+            
+            /* Add a click handler for the delete row */
+            $('#delete').click( function() {
+              var anSelected = fnGetSelected( oTable );
+              oTable.fnDeleteRow( anSelected[0] );
+            } );
+            
+            /* Init the table */
+            oTable = $('#activelist').dataTable( );
+          } );
+
+
+          /* Get the rows which are currently selected */
+          function fnGetSelected( oTableLocal )
+          {
+            var aReturn = new Array();
+            var aTrs = oTableLocal.fnGetNodes();
+            
+            for ( var i=0 ; i<aTrs.length ; i++ )
+            {
+              if ( $(aTrs[i]).hasClass('row_selected') )
+              {
+                aReturn.push( aTrs[i] );
+              }
+            }
+            return aReturn;
+          }
